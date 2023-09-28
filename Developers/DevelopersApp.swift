2f9a9b7494bd8c7dc9323cc051e370a0ef0fd2d1
aungkyawmon1 <aungkyawmon.am@gmail.com>
2023-09-28 09:41:58 +0630

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by solinx on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
